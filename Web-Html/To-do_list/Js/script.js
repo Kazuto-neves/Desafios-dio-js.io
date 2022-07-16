c9889f04fcf40935c9732eb2ad8507302fdbc22b
Kazuto-neves <chrismneves@gmail.com>
2022-07-16 12:19:42 -0300

    to-do list completo

--- Web-Html/To-do_list/Js/script.js
+++ Web-Html/To-do_list/Js/script.js
@@ -1,20 +1,29 @@
-function addRotina(){
-    var n=1;
-    document.getElementById("btnRotina").addEventListener("click",function(){
-        var lista=document.getElementById("ListRotina");
-        var Rotina=document.getElementById("textRotina");
-        var checkbox=document.createElement("input");
-        var texto=document.createElement("label");
-        checkbox.type="checkbox";
-        checkbox.setAttribute("class","rotina");
-        checkbox.setAttribute("id","id"+n.toString());
-        texto.setAttribute("class","lRotina");
-        texto.setAttribute("for","id"+n.toString());
-        texto.textContent=Rotina.value;
-        lista.appendChild(checkbox);
-        lista.appendChild(texto);
-        Rotina.value="";
-        Rotina.focus();
-        n++;
-    })
+const form = document.getElementById('task-form');
+const taskList = document.getElementById('tasks');
+
+form.onsubmit = function (e) {
+	e.preventDefault();
+	const inputField = document.getElementById('task-input');
+	addTask(inputField.value);
+	form.reset();
+};
+
+function addTask(description) {
+	const taskContainer = document.createElement('div');
+	const newTask = document.createElement('input');
+	const taskLabel = document.createElement('label');
+	const taskDescriptionNode = document.createTextNode(description);
+
+	newTask.setAttribute('type', 'checkbox');
+	newTask.setAttribute('name', description);
+	newTask.setAttribute('id', description);
+
+	taskLabel.setAttribute('for', description);
+	taskLabel.appendChild(taskDescriptionNode);
+
+	taskContainer.classList.add('task-item');
+	taskContainer.appendChild(newTask);
+	taskContainer.appendChild(taskLabel);
+
+	taskList.appendChild(taskContainer);
 }
